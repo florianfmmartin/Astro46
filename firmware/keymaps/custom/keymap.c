3769b9fa0a8abfbe50bcc5eac85d0bdf665e0f12
florianfmmartin <florian.fm.martin@gmail.com>
2020-10-20 10:34:29 -0400

    good firmware

--- firmware/keymaps/custom/keymap.c
+++ firmware/keymaps/custom/keymap.c
@@ -4,8 +4,7 @@
 enum astro46_layers {
 	_DEFAULT,
 	_NUMBERS,
-	_FUNC,
-	_ACCENT,
+	_FNCTION,
 };
 
 #define _DEFAULT 0
@@ -21,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 						KC_LSFT, KC_Z,         KC_X,    KC_C,   KC_V,    KC_B,
 						KC_N,    KC_M,         KC_COMM, CA_DOT, KC_SLSH, CA_EGRV,
 						KC_LGUI, MO(_NUMBERS), KC_ENT,
-						KC_SPC,  MO(_FUNC),    KC_LALT),
+						KC_SPC,  MO(_FNCTION),    KC_LALT),
 
 	[_NUMBERS] = KEYMAP(KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
 						KC_TRNS,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
